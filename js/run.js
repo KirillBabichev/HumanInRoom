191d7a05b0e6aa5715f42ea98fd9bd165dff3898
kirill.babichev <kirill.babichev@gmail.com>
2018-08-24 02:00:42 +0300

    bug fixes

--- js/run.js
+++ js/run.js
@@ -1,46 +1,71 @@
 /**
  * Created by Peache on 23.08.2018.
  */
-// create room
+
+// drawing a room
+
 window.onload = function () {
     for (let i = 0; i < 49; i++) {
         document.getElementById('room').innerHTML += "<div class='block'></div>";
     }
 };
 
+    let human = document.getElementById('human');
+    let humanHorizontalMove = 0;
+    let humanVerticalMove = 0;
 
-let human = document.getElementsByClassName('human');
-let humanLeft = 0;
-let humanTop = 0;
-let humanDown = 0;
-let humanRight = 0;
+    document.onkeydown = function (e) {
+        console.log(e.keyCode);
+        // Up = 38
+        // down = 40
+        // left = 37
+        // right =39
 
-document.onkeydown  = function (e) {
-    console.log("test");
-    if (e.key =='ArrowRight'){
-        human.style.left = humanLeft + 'px';
-        humanLeft++;
+        // move Right
+        if (e.keyCode == 39) {
+            humanHorizontalMove += 92;
+            human.style.left = humanHorizontalMove + 'px';
+            horizontalValidator(human.style.left);
+
+            // move Left
+        }else if (e.keyCode == 37) {
+            humanHorizontalMove -= 92;
+            human.style.left = humanHorizontalMove + 'px';
+            horizontalValidator(human.style.left);
+
+            // move Down
+        }else if (e.keyCode == 40) {
+            humanVerticalMove += 92;
+            human.style.top = humanVerticalMove + 'px';
+            verticalValidator(human.style.top);
+            console.log(human.style.top);
+
+            // move Up
+        }else if (e.keyCode == 38){
+            humanVerticalMove -= 92;
+            human.style.top = humanVerticalMove + 'px';
+            verticalValidator(human.style.top);
         }
-};
+    };
 
-document.onkeydown = function (e) {
-    console.log("test");
-    if (e.key =='ArrowDown'){
-        human.style.top = humanTop + 'px';
-        humanTop++;
+    // validator
+    function horizontalValidator(position) {
+        if (parseInt(position) >= 553) {
+            human.style.left = "552px";
+        } else if (parseInt(position) <= 93) {
+            human.style.left = "92px";
+        }
     }
-};
+    function verticalValidator(position) {
+        if (parseInt(position) >= 369){
+            human.style.top = "368px";
+        } else if (parseInt(position) <= 93) {
+            human.style.top = "92px";
+        }
+    }
 
-document.onkeydown = function (e) {
-    if (e.key =='ArrowLeft'){
-        human.style.left =  humanRight + 'px';
-        --humanRight;
-    }
-};
+    //rotate
+    function rotate(deg) {
+        human.setAttribute("style","transform :rotate("+deg+"deg)");
 
-document.onkeydown = function (e) {
-    if (e.key =='ArrowUp'){
-        human.style.top = humanDown + 'px';
-        --humanDown;
     }
-};
